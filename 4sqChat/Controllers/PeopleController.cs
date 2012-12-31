3d4a119276e267eb1e545a861e6b4c1d58fa5b45
Rodion Shkrobot <rodion987@gmail.com>
2012-12-31 19:18:14 +0200

    Second Api part

---
+++ 4sqChat/Controllers/PeopleController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using _4sqChat.Logic;
+using _4sqChat.Models;
+
+namespace _4sqChat.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        private FoursquareOAuth foursquareOAuth;
+        private string CurrentUserToken()
+        {
+            if (!User.Identity.IsAuthenticated)
+                return null;
+            Models.FoursquareUserContext fsqDBContext = new FoursquareUserContext();
+            Models.FoursquareUserModel um = fsqDBContext.FoursquareUsers.Find(Convert.ToInt32(User.Identity.Name));
+            if (um != null)
+                return um.Token;
+            return null;
+        }
+        private void InitializeOauth()
+        {
+            foursquareOAuth = new FoursquareOAuth(CurrentUserToken());
+        }
+
+        public List<Profile> GetNearbyUsers()
+        {
+            InitializeOauth();
+            List<int> nearbyUsersIds = foursquareOAuth.GetNearByUsers();
+            List<Profile> nearbyUsers = new List<Profile>();
+            foreach (var nearbyUsersId in nearbyUsersIds)
+            {
+                nearbyUsers.Add(foursquareOAuth.GetProfileInfo(nearbyUsersId));
+            }
+            return nearbyUsers;
+        }
+        public Profile GetNearbyUserById(int id)
+        {
+            InitializeOauth();
+            return foursquareOAuth.GetProfileInfo(id);
+        }
+    }
+}
