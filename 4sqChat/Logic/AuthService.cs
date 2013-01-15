1c2eefdeb157354dbe3f3a7dcf1f3363216be32f
Vladimir <vladimir.sinkov@gmail.com>
2013-01-16 00:15:34 +0200

    AuthService to Authenticate API users

---
+++ 4sqChat/Logic/AuthService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using _4sqChat.Models;
+
+namespace _4sqChat.Logic
+{
+    public class AuthService
+    {
+        public static bool ValidateAuthData(int userId, string token)
+        {
+            FoursquareUserContext dbContent = new FoursquareUserContext();
+            FoursquareUserModel user = dbContent.FoursquareUsers.Find(userId);
+
+            if (user == null)
+                return false;
+
+            if (user.FoursquareUserId == userId && token == user.Token)
+                return true;
+            return false;
+        }
+    }
+}
